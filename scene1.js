b0e840fbf80985cca90876a39b6b5eb3448ae68a
Rowan Brown <rowanbrown644@gmail.com>
2023-04-17 16:16:07 -0400

    fixed typo issues and added a switch to scene 2

--- scene1.js
+++ scene1.js
@@ -4,6 +4,7 @@ class scene1 extends Phaser.Scene {
     }
 
     create(){
-        this.add.text(20,20,"Loading game...")
+        this.add.text(20,20,"Loading game...");
+        this.scene.start("playGame");
     }
 }
