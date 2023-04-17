47e01ef45a0b996e99198672a12722b59487921a
Rowan Brown <rowanbrown644@gmail.com>
2023-04-17 16:22:54 -0400

    removed the scaling window data. seems to be causing issues?

--- Game.js
+++ Game.js
@@ -1,8 +1,8 @@
 window.onload = function() {
 
     var config = {
-        width: window.innerWidth,
-        height: window.innerHeight,
+        width: 800, //window.innerWidth,
+        height: 600, //window.innerHeight,
         backgroundColor: 0x191970,
         scene: [scene1]
     }
